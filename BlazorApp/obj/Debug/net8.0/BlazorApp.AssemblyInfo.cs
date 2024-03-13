fd406cb02912a7199eec8f6d59c80ae412a62257
Archie Verma <a1745070@student.adelaide.edu.au>
2024-03-13 15:32:20 +1030

    cleaned frontend

--- BlazorApp/obj/Debug/net8.0/BlazorApp.AssemblyInfo.cs
+++ BlazorApp/obj/Debug/net8.0/BlazorApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f72bbfa085b0c1dc4909bcbc261ff5069df841a2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7fb97f6adc6b3af040a4e3c384b05191433dfe9e")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
